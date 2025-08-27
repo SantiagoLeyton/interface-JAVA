8e7fa374397944f518c2902fc57197c8fa04842d
MarianaGonzalez <nmgonzalez_645@unihumboldt.edu.co>
2025-08-27 18:40:45 -0500

    finished 'enchufable'

---
+++ src/Enchufable/enchufable.java
@@ -0,0 +1,61 @@
+package Enchufable;
+
+interface Enchufable {
+    void Encender();
+    void Apagar();
+    default boolean EstaEncendido(boolean estado) {
+        return estado;
+    };
+}
+
+class Lampara implements Enchufable {
+
+    boolean estado;
+
+    @Override
+    public void Encender() {
+        if (estado) {
+            System.out.println("La lampara ya estaba encendida");
+        }
+        else {
+            System.out.println("La lampara se encendió correctamente");
+            EstaEncendido(true);
+        }
+    }
+
+    @Override
+    public void Apagar() {
+        if (!estado) {
+            System.out.println(("La lampara no esta encendida!"));
+        } else {
+            System.out.println(("La lampara se apagó correctamente"));
+            EstaEncendido(false);
+        }
+    }
+}
+
+class Ventilador implements Enchufable {
+
+    boolean estado;
+
+    @Override
+    public void Encender() {
+        if (estado) {
+            System.out.println("El ventilador ya estaba encendido");
+        }
+        else {
+            System.out.println("El ventilador se encendió correctamente");
+            EstaEncendido(true);
+        }
+    }
+
+    @Override
+    public void Apagar() {
+        if (!estado) {
+            System.out.println(("El ventilador no esta encendido!"));
+        } else {
+            System.out.println(("El ventilador se apagó correctamente"));
+            EstaEncendido(false);
+        }
+    }
+}
